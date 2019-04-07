285f74b7aec994bde8a53c08fe63922458534743
tomic <violeticus@gmail.com>
2019-04-07 09:03:26 +0200

    Fixing test class from WeatherAlarmNotifier

--- 06_first-integration-tests/src/test/java/foo/bar/meteorology/weather/alarm/WeatherAlarmNotifierTest.java
+++ 06_first-integration-tests/src/test/java/foo/bar/meteorology/weather/alarm/WeatherAlarmNotifierTest.java
@@ -17,35 +17,4 @@ public class WeatherAlarmNotifierTest {
             );
 
     //here comes your tests
-<<<<<<< Updated upstream
-=======
-    @Test
-    public void testWeatherAlarmNotifier() {
-        WeatherAlarmLevel weatherAlarmLevel = alarmNotifier.raiseLevelIfNeccessary();
-
-        Assertions.assertEquals(WeatherAlarmLevel.ALL_OK, weatherAlarmLevel);
-    }
-
-    WeatherService weatherMock = Mockito.mock(WeatherService.class);
-
-    private WeatherAlarmNotifier alarmNotifierWithMock
-            = new WeatherAlarmNotifier(weatherMock);
-
-    @ParameterizedTest(name = "When weatherService return " +
-            "Weather (temp = {0} C, wind = {1} km/h, rain = {2} mm/day) " +
-            "expected alarm status should be equal {3}")
-    @CsvSource({
-            "25, 10, 5, ALL_OK",
-            "25, 100, 5, SECOND_LEVEL"
-    })
-    public void testWeatherAlarmNotifierTest(int temp, int wind, int rain, WeatherAlarmLevel expectedAlarm) {
-        Mockito.when(weatherMock.getWeather())
-                .thenReturn(new Weather(temp, wind, rain));
-
-        WeatherAlarmLevel weatherAlarmLevel = alarmNotifierWithMock.raiseLevelIfNeccessary();
-
-        Assertions.assertEquals(expectedAlarm, weatherAlarmLevel);
-    }
->>>>>>> Stashed changes
-
 }
